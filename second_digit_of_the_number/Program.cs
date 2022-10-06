33583e3acee79dfe77ee7f89bf5a653347e95dd2
Ogrimor <ogrimors37@gmail.com>
2022-10-06 09:42:51 +0500

    Second_digit_of_the_number

---
+++ second_digit_of_the_number/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите трёхзначное число: ");
+string s = Console.ReadLine();
+int number = int.Parse(s);
+int twoLastDigit = number % 100;
+int secondDigit = twoLastDigit / 10;
+Console.WriteLine(secondDigit);
