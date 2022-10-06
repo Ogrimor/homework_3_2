090c8b63be7317e016032ed5c46445f1086b97b0
Ogrimor <ogrimors37@gmail.com>
2022-10-06 10:42:41 +0500

    Checking_the_day_of_the_week

---
+++ checking_the_day_of_the_week/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите желаемый день недели, от 1 до 7: ");
+string s = Console.ReadLine();
+int numberDay = int.Parse(s);
+if(numberDay > 0 && numberDay < 8)
+{
+    if(numberDay == 6) Console.WriteLine("Yes");
+    if(numberDay == 7) Console.WriteLine("Yes");
+    else Console.WriteLine("No");
+}
+else
+{
+    Console.WriteLine("Введите число от 1 до 7");
+}
