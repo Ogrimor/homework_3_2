9198ab20558f1b15a203101a9c91f14f7f81634d
Ogrimor <ogrimors37@gmail.com>
2022-10-06 10:56:27 +0500

    Add correction

--- third_digit_of_the_number/Program.cs
+++ third_digit_of_the_number/Program.cs
@@ -2,11 +2,5 @@
 string number = Console.ReadLine();
 char[] removeMinus = {'-'};
 string positiveNumber = number.TrimStart(removeMinus);
-if (positiveNumber.Length > 3)
-{
-    Console.WriteLine(positiveNumber[2]);
-}
-else
-{
-    Console.WriteLine("Третьей цифры нет");
-}
+if (positiveNumber.Length > 2) Console.WriteLine(positiveNumber[2]);
+else Console.WriteLine("Третьей цифры нет");
