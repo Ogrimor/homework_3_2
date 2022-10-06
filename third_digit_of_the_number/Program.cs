99f2a974db97f777b1e6cbe69448a7559d2319a3
Ogrimor <ogrimors37@gmail.com>
2022-10-06 10:32:56 +0500

    Third_digit_of_the_number

---
+++ third_digit_of_the_number/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число: ");
+string number = Console.ReadLine();
+char[] removeMinus = {'-'};
+string positiveNumber = number.TrimStart(removeMinus);
+if (positiveNumber.Length > 3)
+{
+    Console.WriteLine(positiveNumber[2]);
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
